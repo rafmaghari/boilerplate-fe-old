2c81f83f7311251ae409fcba15eb5ed76d1211fc
rafmaghari <29rafaelmaghari@gmail.com>
2023-02-11 02:36:38 +0800

    [auth] sign out

--- components/Common/Auth/AuthSidebar/AuthSideBarItem.tsx
+++ components/Common/Auth/AuthSidebar/AuthSideBarItem.tsx
@@ -4,16 +4,25 @@ import  Link  from 'next/link'
 interface IProps {
     children: any,
     label: string,
-    route: string,
+    route?: string,
+    onClickFunction?: (event: React.ChangeEvent<HTMLInputElement>) => void
 }
 
-const AuthSideBarItem = ({children, label, route}: IProps) => {
-    return (
-        <Link href={route} className="inline-block w-full h-full px-3 py-2 font-bold text-white">
+const AuthSideBarItem = ({children, label, route, onClickFunction}: IProps) => {
+    let display;
+    if (route) {
+        display =<Link href={route} className="inline-block w-full h-full px-3 py-2 font-bold text-white">
             {children}
             {label}
         </Link>
-    );
+    } else {
+        display =<a href="#" className="inline-block w-full h-full px-3 py-2 font-bold text-white" onClick={() => onClickFunction}>
+            {children}
+            {label}
+        </a>
+
+    }
+    return display
 };
 
 export default AuthSideBarItem;
--- components/Common/Auth/AuthSidebar/index.tsx
+++ components/Common/Auth/AuthSidebar/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import AuthSideBarItem from "./AuthSideBarItem";
 import SearchInput from "./SearchInput";
+import {signOut} from "next-auth/react";
+import {log} from "util";
 
 const AuthSidebar = () => {
     const title = `${process.env.NEXT_PUBLIC_APP_NAME} - Admin`
@@ -27,13 +29,25 @@ const AuthSidebar = () => {
                             </svg>
                         </AuthSideBarItem>
                     </li>
-                    <li className="mb-2 bg-blue-500 rounded shadow">
+                    <li className="mb-2 rounded hover:shadow hover:bg-blue-800">
                         <AuthSideBarItem label="Profile" route="/me" >
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5"
                                  stroke="currentColor" className="inline-block w-6 h-6 mr-2 -mt-2">
                                 <path strokeLinecap="round" strokeLinejoin="round"
                                       d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"/>
                             </svg>
+
+                        </AuthSideBarItem>
+                    </li>
+                    <li className="mb-2 rounded hover:shadow hover:bg-blue-800">
+                        <AuthSideBarItem label="">
+                            <button onClick={() => signOut()}>
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5"
+                                     stroke="currentColor" className="inline-block w-6 h-6 mr-2 -mt-2">
+                                    <path strokeLinecap="round" strokeLinejoin="round"
+                                          d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75"/>
+                                </svg>Sign out
+                            </button>
 
                         </AuthSideBarItem>
                     </li>
