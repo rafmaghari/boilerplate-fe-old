8939d15074316474dc4d920a6c5623cc779654b7
rafmaghari <29rafaelmaghari@gmail.com>
2023-02-15 01:11:41 +0800

    [task] add pagination

--- components/Common/Form/Button/index.tsx
+++ components/Common/Form/Button/index.tsx
@@ -3,25 +3,20 @@ import React from 'react';
 interface IProps  {
     label: string;
     size?: string
-    variant: 'info' | 'danger' | 'warning'
+    variant: string
+    onClick: (event: React.MouseEvent<HTMLButtonElement>) => void
 }
-const Button = ({label, size = 'px-12 py-3', variant}: IProps) => {
-    let buttonVariant = '';
-    if (variant === 'warning') {
-        buttonVariant = 'yellow'
-    } else if (variant === 'danger') {
-        buttonVariant = 'red'
-    } else {
-       buttonVariant = 'blue'
-    }
+const Button = ({label, size = 'px-12 py-3', variant, onClick}: IProps) => {
     return (
-        <button
-                className={`flex inline-block shrink-0 rounded-md border border-${buttonVariant}-600
-                bg-${buttonVariant}-600 ${size} text-sm font-medium text-white
-                transition hover:bg-transparent hover:text-${buttonVariant}-600 focus:outline-none focus:ring active:text-${buttonVariant}-500`}
-        >
-            {label}
-        </button>
+            <button
+                className={`flex inline-block shrink-0 rounded-md border border-${variant}-600
+               bg-${variant}-600 ${size} text-sm font-medium text-white
+                transition hover:bg-transparent hover:text-${variant}-600
+                focus:outline-none focus:ring active:text-${variant}-500`}
+                onClick={onClick}
+            >
+                {label}
+            </button>
     );
 };
 
--- pages/tasks/index.tsx
+++ pages/tasks/index.tsx
@@ -3,13 +3,14 @@ import AuthLayout from "../../components/Layout/Auth";
 import {getSession} from "next-auth/react";
 import HttpServerService from "../../services/http-server.service";
 import {ITask} from "../../types/Task";
-import { useTable } from 'react-table'
+import {usePagination, useTable} from 'react-table'
 import Button from "../../components/Common/Form/Button";
 
 export const getServerSideProps = async ({req}: any) => {
     const session = await getSession({req}) as any;
     const {data} = await HttpServerService.getServer('tasks', session.access_token)
     const tasks = data.data;
+
     return {
         props: { tasks },
     }
@@ -18,11 +19,6 @@ export const getServerSideProps = async ({req}: any) => {
 interface IProps {
     tasks: ITask[]
 }
-
-
-// React Table
-
-
 
 const Task = ({tasks}: IProps): JSX.Element => {
 
@@ -47,12 +43,12 @@ const Task = ({tasks}: IProps): JSX.Element => {
             {
                 Header: 'Action',
                 accessor: "action",
-                Cell: (tableInstance) => {
+                Cell: (tableInstance: any) => {
                     const { row: index } = tableInstance;
                     return (
                         <div className="flex space-x-1">
-                            <Button label="Edit" size="px-5 py-2" variant="info" />
-                            <Button label="Delete" size="px-5 py-2" variant="warning" />
+                            <Button label="Edit" size="px-5 py-2" variant="blue" onClick={() => alert('edit')} />
+                            <Button label="Delete" size="px-5 py-2" variant="red" onClick={() => alert('delete')} />
                         </div>
                 )
                 }
@@ -60,42 +56,61 @@ const Task = ({tasks}: IProps): JSX.Element => {
         ],
         []
     )
-    const Table = ({columns, data}) => {
-        const { getTableProps, getTableBodyProps, headerGroups,rows, prepareRow } = useTable({columns, data,})
+    const Table = ({columns, data}: any) => {
+
+        const {
+            getTableProps,
+            getTableBodyProps,
+            headerGroups,
+            // @ts-ignore
+            page,
+            // @ts-ignore
+            nextPage,
+            // @ts-ignore
+            previousPage,
+            prepareRow
+        } = useTable({columns, data}, usePagination)
 
         return (
-            <table className="min-w-full text-center " {...getTableProps()}>
-               <thead className="border-b bg-gray-800">
-                 {headerGroups.map((headerGroup, key) => (
-                     <tr {...headerGroup.getHeaderGroupProps()} key={key}>
-                         {headerGroup.headers.map((column, key) => (
-                             <th
-                                 {...column.getHeaderProps()} key={key} className="text-sm font-medium text-white px-6 py-4"
-                             >
-                                 {column.render('Header')}
-                             </th>
-                         ))}
-
-                     </tr>
-                 ))}
-               </thead>
-                <tbody {...getTableBodyProps()}>
-                {rows.map((row, i) => {
-                    prepareRow(row)
-                    return (
-                        <tr {...row.getRowProps()} key={row.id} className="border-b">
-                            {row.cells.map((cell, key) => {
-                                return <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap"
-                                           {...cell.getCellProps()} key={key}
+            <div>
+                <table className="min-w-full text-center " {...getTableProps()}>
+                    <thead className="border-b bg-gray-800">
+                    {headerGroups.map((headerGroup, key) => (
+                        <tr {...headerGroup.getHeaderGroupProps()} key={key}>
+                            {headerGroup.headers.map((column, key) => (
+                                <th
+                                    {...column.getHeaderProps()} key={key} className="text-sm font-medium text-white px-6 py-4"
                                 >
-                                    {cell.render('Cell')}
-                                </td>
-                            })}
+                                    {column.render('Header')}
+                                </th>
+                            ))}
+
                         </tr>
-                    )
-                })}
-                </tbody>
-            </table>
+                    ))}
+                    </thead>
+                    <tbody {...getTableBodyProps()}>
+                    {page.map((row: any, i: number) => {
+                        prepareRow(row)
+                        return (
+                            <tr {...row.getRowProps()} key={row.id} className="border-b">
+                                {row.cells.map((cell: any, key: number) => {
+                                    return <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap"
+                                               {...cell.getCellProps()} key={key}
+                                    >
+                                        {cell.render('Cell')}
+                                    </td>
+                                })}
+                            </tr>
+                        )
+                    })}
+                    </tbody>
+                </table>
+                <div className="flex space-x-1 my-5 justify-end mx-10">
+                    <Button label="Previous" size="px-5 py-2" variant="blue" onClick={() => previousPage()} />
+                    <Button label="Next" size="px-5 py-2" variant="blue" onClick={() => nextPage()} />
+                </div>
+            </div>
+
         )
     }
     return (
